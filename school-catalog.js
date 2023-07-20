8516af9381ee38ae3abf6440c711fc80abd9b53e
Laurie M. Rauch <laurierauch@users.noreply.github.com>
2023-07-20 18:41:50 -0400

    Create school-catalog.js

---
+++ school-catalog.js
@@ -0,0 +1,72 @@
+class School {
+  constructor(name,level,numberOfStudents) {
+    this._name = name;
+    this._level = level;
+    this._numberOfStudents = numberOfStudents;
+  }
+
+  get name() {
+    return this._name;
+  }
+  get level() {
+    return this._level;
+  }
+  get numberOfStudents() {
+    return this._numberOfStudents;
+  }
+
+  set numberOfStudents(num) {
+    if (num.isNaN()){
+      this._numberOfStudents = num;
+    } else {
+      this._numberOfStudents = 'That is Not a Number!';
+      console.log('Invalid input: numberOfStudents must be set to a Number.');
+    }
+  }
+
+  quickFacts() {
+    console.log(`${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`);
+  }
+
+  static pickSubstituteTeacher(){
+    const substituteTeachers = [];
+    const randomNumber = Math.floor(substituteTeachers.length - 1 * Math.random());
+    return substituteTeachers[randomNumber];
+  }
+}
+
+class PrimarySchool extends School {
+  constructor(name, numberOfStudents, pickupPolicy) {
+    super(name);
+    this._level = 'primary';
+    this._numberOfStudents = numberOfStudents;
+    this._pickupPolicy = pickupPolicy;
+  }
+
+  get pickupPolicy() {
+    return this._pickupPolicy;
+  }
+}
+
+class Middle extends School {
+  constructor(name,numberOfStudents) {
+    super(name,numberOfStudents);
+    this._level = 'Middle';
+  }
+}
+
+class High extends School {
+  constructor(name,numberOfStudents,sportsTeams) {
+    super(name,numberOfStudents);
+    this._sportsTeams = sportsTeams;
+    this._level = "High"
+  }
+
+  get sportsTeams() {
+    return this._sportsTeams;
+  }
+}
+
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 'George', 'Students must be picked up by a parent, guardian or a family member over the age of 13.');
+
+lorraineHansbury.quickFacts();
